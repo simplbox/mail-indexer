03e53ca698643670104f64bf3932040bb0684651
Gunjan Nandy <gunjan.nandy1@gmail.com>
2023-02-02 17:37:50 +0530

    Spring init

---
+++ src/main/java/com/simplbox/mailindexer/MailIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.simplbox.mailindexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailIndexerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplbox/mailindexer/MailIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplbox.mailindexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailIndexerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
